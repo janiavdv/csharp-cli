3f4b5a3037ecef9899a633ad465189c358436422
Jania Vandevoorde <jania@vdvoorde.com>
2024-06-07 16:48:12 -0400

    Map attributes to a BSON field name

--- BookRecommender/Book.cs
+++ BookRecommender/Book.cs
@@ -1,22 +1,40 @@
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace BookRecommender;
 
 public class Book
 {
+    [BsonElement("AverageRating")]
     public float AverageRating { get; set; }
+    [BsonElement("Description")]
     public string Description { get; set; }
+    [BsonElement("Genres")]
     public List<string> Genres { get; set; }
+    [BsonElement("ImageURL")]
     public string ImageURL { get; set; }
+    [BsonElement("LanguageCode")]
     public string LanguageCode { get; set; }
+    [BsonElement("OriginalPublicationYear")]
     public int? OriginalPublicationYear { get; set; }
+    [BsonElement("OriginalTitle")]
     public string OriginalTitle { get; set; }
+    [BsonElement("NumPages")]
     public int? NumPages { get; set; }
+    [BsonElement("Ratings1")]
     public int Ratings1 { get; set; }
+    [BsonElement("Ratings2")]
     public int Ratings2 { get; set; }
+    [BsonElement("Ratings3")]
     public int Ratings3 { get; set; }
+    [BsonElement("Ratings4")]
     public int Ratings4 { get; set; }
+    [BsonElement("Ratings5")]
     public int Ratings5 { get; set; }
+    [BsonElement("NumRatings")]
     public int NumRatings { get; set; }
+    [BsonElement("Title")]
     public string Title { get; set; }
+    [BsonElement("Authors")]
     public List<string> Authors { get; set; }
 
     public Book(JSONBook rawData)
