1f9880460c860a616f7922b2fc1aff401b428b95
Jania Vandevoorde <jania@vdvoorde.com>
2024-06-11 10:49:19 -0400

    Refactor code to *only* quit upon user request

--- BookRecommender/Program.cs
+++ BookRecommender/Program.cs
@@ -11,17 +11,11 @@ class Program
     {
         Console.WriteLine("Enter name: ");
         string? username = Console.ReadLine();
-        Console.WriteLine("Hi, " + username + ". Welcome to the Book Recommender.");
-        Console.WriteLine("Do you want to reset the database? ('y' or 'n')");
+        Console.WriteLine($"Hi, {username}. Welcome to the Book Recommender.");
+        Console.WriteLine("Do you want to reset the database? ('y' for yes / any other key for no)");
         string resetStr = Console.ReadLine();
-        if (resetStr != "y" && resetStr != "n")
-        {
-            Console.WriteLine("\""+ resetStr + "\" was not one of the choices. Application quitting...");
-            return; 
-        }
-
         bool reset = resetStr == "y";
-        
+
         // Sets the connection URI
         const string connectionUri = "mongodb://localhost:27017/";
         // Creates a new client and connects to the server
@@ -34,7 +28,7 @@ class Program
             if (reset || collection.EstimatedDocumentCount() > 10000)
                 // Delete all documents in the database
                 collection.DeleteMany(Builders<Book>.Filter.Empty);
-            
+
             const string path = "../../../data/books.json";
             List<JSONBook>? jsonData;
 
@@ -53,10 +47,10 @@ class Program
 
             // ensure all books were successfully converted
             Debug.Assert(jsonData.Count == books.Count);
-        
+
             collection.InsertMany(books);
         }
-        
+
         while (true) // App continues until user quits
         {
             Console.WriteLine("Do you want to find a book ('f'), update your read books ('u'), or quit the app ('q')?");
@@ -69,120 +63,119 @@ class Program
                     return;
                 case "f":
                 {
-                    Console.WriteLine("How do you want to find a book today?");
-                    Console.WriteLine("Choose from these options:");
-
+                    
                     List<string> options = new List<string>
                         { "OriginalTitle", "Authors", "Genres", "OriginalPublicationYear", "LanguageCode" };
-                    foreach (string option in options)
-                        Console.WriteLine("- " + option);
-
-                    string column = Console.ReadLine();
-
-                    if (!options.Contains(column))
+                    string column;
+                    
+                    while (true)
                     {
-                        Console.WriteLine(
-                            "\"" + column + "\" was not one of the specified options. Application quitting...");
-                        return;
+                        Console.WriteLine("How do you want to find a book today?");
+                        Console.WriteLine("Choose from these options:");
+                        foreach (string option in options)
+                            Console.WriteLine($"- {option}");
+
+                        column = Console.ReadLine();
+
+                        if (options.Contains(column))
+                            break;
+
+                        Console.WriteLine($"\"{column}\" was not one of the specified options. Try again.");
                     }
-
-                    Console.WriteLine("What " + column + " are you looking for?");
-                    string search = Console.ReadLine();
-
+                    
+                    string search;
                     FilterDefinitionBuilder<Book> builder = Builders<Book>.Filter;
-                    FilterDefinition<Book> filter;
-
-                    switch (column)
+                    FilterDefinition<Book>? filter = null;
+                    bool validSearch = false;
+                    
+                    while (!validSearch)
                     {
-                        case "OriginalTitle":
-                        case "Authors":
-                        case "Genres":
+                        Console.WriteLine($"What {column} are you looking for?");
+                        search = Console.ReadLine();
+
+                        if (new List<string>{ "OriginalTitle", "Authors", "Genres"}.Contains(column))
                         {
                             filter = builder
-                                .Regex(column, new BsonRegularExpression(".*" + search + ".*"));
-                            break;
+                                .Regex(column, new BsonRegularExpression($".*{search}.*"));
+                            validSearch = true;
                         }
-                        case "OriginalPublicationYear":
+                        if (column == "OriginalPublicationYear")
                         {
                             try
                             {
                                 int year = Convert.ToInt32(search);
                                 filter = builder
                                     .Eq(column, year);
+                                validSearch = true;
                             }
                             catch (Exception e)
                             {
-                                Console.WriteLine("Invalid year: \"" + search + "\". Application quitting...");
-                                return;
+                                Console.WriteLine($"Invalid year: {search}. Try again.");
                             }
-
-                            break;
                         }
-                        case "LanguageCode":
+                        if (column == "LanguageCode")
                         {
                             filter = builder
                                 .Eq(column, search);
+                            validSearch = true;
+                        }
+                    }
+
+                    int count;
+                    while (true)
+                    {
+                        Console.WriteLine("How many results (at most) do you want?");
+                        string countStr = Console.ReadLine();
+                        try
+                        {
+                            count = Convert.ToInt32(countStr);
                             break;
                         }
-                        default:
-                            // This block is never entered since we verify validity of column above.
-                            return;
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Invalid integer {countStr}. Try again.");
+                        }
                     }
-
-
-                    Console.WriteLine("How many results (at most) do you want?");
-                    string countStr = Console.ReadLine();
-                    int count = 0;
-                    try
-                    {
-                        count = Convert.ToInt32(countStr);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Invalid integer \"" + countStr + "\". Application quitting...");
-                        return;
-                    }
-
+                    
+                    Debug.Assert(filter != null);
                     var booksFound = collection
                         .Find(filter)
                         .Limit(count)
                         .ToList();
 
                     if (booksFound.Count == 0)
-                    {
-                        Console.WriteLine("No books found. Application quitting...");
-                        return;
-                    }
+                        Console.WriteLine("No books found.");
 
                     foreach (Book bookFound in booksFound)
                         bookFound.PrintBook();
+                    
                     break;
                 }
                 case "u":
                 {
-                    Console.WriteLine("How do you want to search for the book you've read?");
-                    Console.WriteLine("Choose from these options:");
-
-                    List<string> options = new List<string>
-                        { "OriginalTitle", "Authors" };
-                    foreach (string option in options)
-                        Console.WriteLine("- " + option);
-
-                    string column = Console.ReadLine();
-
-                    if (!options.Contains(column))
+                    List<string> options = new List<string> { "OriginalTitle", "Authors" };
+                    string column;
+                    while (true)
                     {
-                        Console.WriteLine(
-                            "\"" + column + "\" was not one of the specified options. Application quitting...");
-                        return;
+                        Console.WriteLine("How do you want to search for the book you've read?");
+                        Console.WriteLine("Choose from these options:");
+                        foreach (string option in options)
+                            Console.WriteLine($"- {option}");
+                        
+                        column = Console.ReadLine();
+
+                        if (options.Contains(column))
+                            break;
+                        
+                        Console.WriteLine($"\"{column}\" was not one of the specified options. Try again.");
                     }
 
-                    Console.WriteLine("What " + column + " are you looking for? Be as specific as possible.");
+                    Console.WriteLine($"What {column} are you looking for? Be as specific as possible.");
                     string search = Console.ReadLine();
 
                     FilterDefinition<Book> filter = Builders<Book>
                         .Filter
-                        .Regex(column, new BsonRegularExpression(".*" + search + ".*"));
+                        .Regex(column, new BsonRegularExpression($".*{search}.*"));
 
                     int count = 10;
 
@@ -190,38 +183,41 @@ class Program
                         .Find(filter)
                         .Limit(count)
                         .ToList();
+                    int n = booksFound.Count;
 
-                    if (booksFound.Count == 0)
+                    if (n == 0)
                     {
-                        Console.WriteLine("No books found. Application quitting...");
-                        return;
+                        Console.WriteLine("No books found.");
+                        break;
                     }
 
-                    for (int i = 0; i < booksFound.Count; i++)
-                    {
-                        Book bookFound = booksFound[i];
-                        Console.Write("[" + i + "] ");
-                        bookFound.PrintBookOneLine();
-                    }
-
-                    Console.WriteLine("Which book have you read? Enter the number beside the book.");
-                    string posStr = Console.ReadLine();
                     int pos;
-                    try
+                    while (true)
                     {
-                        pos = Convert.ToInt32(posStr);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Invalid integer \"" + posStr + "\". Application quitting...");
-                        return;
-                    }
+                        for (int i = 0; i < n; i++)
+                        {
+                            Book bookFound = booksFound[i];
+                            Console.Write($"[ {i} ] ");
+                            bookFound.PrintBookOneLine();
+                        }
 
-                    if (pos < 0 || pos > count - 1)
-                    {
-                        Console.WriteLine("Integer " + posStr + " out of range [0, " + (count - 1) +
-                                          "]. Application quitting...");
-                        return;
+                        Console.WriteLine("Which book have you read? Enter the number beside the book.");
+                        string posStr = Console.ReadLine();
+                        try
+                        {
+                            pos = Convert.ToInt32(posStr);
+                            if (pos < 0 || pos > count - 1)
+                            {
+                                Console.WriteLine($"Integer {posStr} out of range [0, {n - 1}]. Try again.");
+                                continue;
+                            }
+
+                            break;
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Invalid integer \"{posStr}\". Try again.");
+                        }
                     }
 
                     Book read = booksFound[pos];
@@ -236,17 +232,18 @@ class Program
                     UpdateResult status = collection.UpdateOne(bookToUpdate, update);
                     if (status.IsAcknowledged && status.ModifiedCount == 1)
                     {
-                        Console.WriteLine("You have read " + read.OriginalTitle + ".");
+                        Console.WriteLine($"You have read {read.OriginalTitle}.");
                     }
                     else
                     {
-                        Console.WriteLine("No updates were made. The book was already marked as read or was not found.");
+                        Console.WriteLine(
+                            "No updates were made. The book was already marked as read or was not found.");
                     }
 
                     break;
                 }
                 default:
-                    Console.WriteLine("Invalid action \"" + action + "\".");
+                    Console.WriteLine($"Invalid action \"{action}\". Try again.");
                     break;
             }
         }
